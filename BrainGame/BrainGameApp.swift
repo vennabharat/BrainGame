cf572505e002f8aa58fdf78cc8601c7cd6fa47ea
Venna Bharat <v.bharath39@gmail.com>
2023-08-22 07:18:34 +0530

    Initial Commit

---
+++ BrainGame/BrainGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainGameApp.swift
+//  BrainGame
+//
+//  Created by bharat venna on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BrainGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
